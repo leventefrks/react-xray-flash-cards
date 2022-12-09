bb11361b09f13fc63b3e48770031cd6bd66a2638
x-farkasle <x-farkasle@wizzair.com>
2022-12-09 23:11:40 +0100

    .env, contentful dynamic data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
+import { client } from './api';
 import ReactCardFlip from 'react-card-flip';
 import { useState } from 'react';
 import { FiRotateCcw } from 'react-icons/fi';
 import { GiCardRandom } from 'react-icons/gi';
-import DATA from './assets/data.json';
-
-const data = DATA;
+import { useEffect } from 'react';
 
 const App = () => {
   const [isFlipped, setFlip] = useState(false);
   const [isToggle, setToggle] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [items, setItems] = useState([]);
+  const [currentItem, setCurrentItem] = useState({});
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const { items } = await client.getEntries({
+          content_type: 'xray',
+        });
+
+        setItems(items);
+        setCurrentItem(items[currentIndex].fields);
+      } catch (error) {
+        console.error('Failed to fetch x-ray items', error);
+      }
+    };
+
+    fetchItems();
+  }, []);
+
+  useEffect(() => {
+    setCurrentItem(items[currentIndex]?.fields);
+  }, [currentIndex]);
 
   return (
     <div className="App">
       <div className="w-full min-h-screen bg-gray-100 px-2 sm:px-0">
-        <h1 className="pt-2 text-2xl font-black text-gray-900 text-center">
+        <h1 className="pt-2 text-xl font-black text-gray-900 text-center">
           X-Ray Memory Test
         </h1>
         <div className="flex flex-col min-h-screen items-center justify-center">
@@ -27,7 +49,10 @@ const App = () => {
               <div className="p-4 h-[500px] w-500 shadow-xl bg-white rounded-md">
                 <img
                   className="max-w-100 h-full object-contain rounded-sm"
-                  src={data[currentIndex].image && data[currentIndex].image}
+                  src={
+                    currentItem?.image?.fields?.file?.url &&
+                    currentItem?.image?.fields?.file?.url
+                  }
                   alt=""
                 />
               </div>
@@ -35,19 +60,19 @@ const App = () => {
               <div className="p-4 h-[500px] w-500 shadow-xl space-y-4 flex flex-col justify-center bg-white rounded-md text-left">
                 <div>
                   <span className="font-bold text-md mr-1">Modalitás:</span>
-                  {data[currentIndex].modality && data[currentIndex].modality}
+                  {currentItem?.modality && currentItem?.modality}
                 </div>
                 <div>
                   <span className="font-bold text-md mr-1">Régió:</span>
-                  {data[currentIndex].region && data[currentIndex].region}
+                  {currentItem?.region && currentItem?.region}
                 </div>
                 <div>
                   <span className="font-bold text-md mr-1">Radiológia:</span>
-                  {data[currentIndex].radiology && data[currentIndex].radiology}
+                  {currentItem?.radiology && currentItem?.radiology}
                 </div>
                 <div>
                   <span className="font-bold text-md mr-1">Diagnózis:</span>
-                  {data[currentIndex].diagnose && data[currentIndex].diagnose}
+                  {currentItem?.diagnose && currentItem?.diagnose}
                 </div>
               </div>
             </ReactCardFlip>
@@ -65,12 +90,13 @@ const App = () => {
             type="button"
             onClick={() =>
               setCurrentIndex(prevIndex =>
-                data.length - 1 <= prevIndex ? 0 : prevIndex + 1
+                items.length - 1 <= prevIndex ? 0 : prevIndex + 1
               )
             }
             className="flex items-center gap-2 mt-4 bg-white border-black border-2 px-4 py-2 transition-colors duration-200 text-gray-900 text-sm rounded-md"
           >
-            <GiCardRandom className="w-3 h-3" /> Gimme another X-Ray
+            <GiCardRandom className="w-3 h-3" />
+            Gimme another X-Ray
           </button>
           {isToggle && (
             <div className="text-gray-900 py-2">
---
+++ src/api/index.js
@@ -0,0 +1,6 @@
+import { createClient } from 'contentful';
+
+export const client = createClient({
+  space: import.meta.env.VITE_SPACE_ID,
+  accessToken: import.meta.env.VITE_ACCESS_TOKEN,
+});
